9cb7d30a37657fb88434fceeb40759f106b3b750
wisphill <fizz.uet@gmail.com>
2026-01-20 10:34:00 +0700

    feat: add TODOs and make the project to be runable

--- Data/ProductRepository.cs
+++ Data/ProductRepository.cs
@@ -7,6 +7,7 @@ namespace LegacyOrderService.Data
 {
     public class ProductRepository
     {
+        // TODO: to check this, should we use double? for finance
         private readonly Dictionary<string, double> _productPrices = new()
         {
             ["Widget"] = 12.99,
@@ -17,6 +18,7 @@ namespace LegacyOrderService.Data
         public double GetPrice(string productName)
         {
             // Simulate an expensive lookup
+            // TODO: check this and consider to use async instead blocking the main thread
             Thread.Sleep(500);
 
             if (_productPrices.TryGetValue(productName, out var price))
